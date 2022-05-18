bd47f64a784a603529fd8bfc4de60e137c097c9d
MARCOSRUIZVIDAURRE <ruizvidaurre40@gmail.com>
2022-05-18 18:21:54 -0500

    [facture] - funciones & operations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import kotlin.math.pow
+
 fun main(){
     //Boolean size 1bit false/true
     //Char size 166bit "a" to "z" "/n" "/101"
@@ -28,4 +30,72 @@ fun main(){
     name = "Marc"
     println("My name is $name $lasName")
 
+    //Operaciones Matematicas
+    //Suma
+    println(2+2)
+    //Resta
+    println(2-2)
+    //Divicion
+    println(2/2)
+    //Modulos
+    println(2%2)
+    //Multiplicacion
+    println(2*2)
+    //Operacion Matematica
+    println(1 + (9 / 3) * 2)
+    //Potencia
+    println(2.0.pow(2))
+
+    //Comparacion e Igualdad operadores
+    //Igual
+    println(2 == 2)
+    //Desigualdad / Diferencia
+    println(2 != 2)
+    // Mayor que
+    println(2 > 2)
+    //Mayor que
+    println(2 < 2)
+    //Mayor o igual que
+    println(2 >= 2)
+    //Menor o igual que
+    println(2 <= 2)
+
+    var anyNumber: Int = 0
+    //operadores de Asignacion
+    anyNumber += 10
+    anyNumber -= 2
+    anyNumber *= 2
+    anyNumber /= 2
+    anyNumber %= 2
+    //Operadores y desincrementador
+    anyNumber++
+    anyNumber--
+    println(anyNumber)
+    //Operadores logicos
+    var isTrue : Boolean = true
+    var isFalse : Boolean = false
+    //Operadores Booleanos logicos
+    //AND
+    println(isTrue && isFalse)
+    // OR
+    println(isTrue || isFalse)
+    //NOT
+    println(!isTrue)
+    helloWorld()
+    var myNumber : Int = dobletheValue(2)
+    println(myNumber)
+    favoriteFoodPrompt("Marcos", "Sushi")
+    favoriteFoodPrompt()
+}
+
+fun helloWorld(){
+    println("Hello World")
+}
+
+fun dobletheValue(numberValue: Int) : Int {
+    return numberValue * 2
+}
+
+fun favoriteFoodPrompt(name : String= "Anonymous", favoriteFood : String = "Pizza") : Unit {
+    println("$name's Favorite food is $favoriteFood")
 }
